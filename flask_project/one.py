1ff1c668a2613785c79dc2f8341df472d7ee3734
Akriti Thakur <112692104+akriti-thakur@users.noreply.github.com>
2025-02-05 16:03:49 +0530

    changes

--- flask_project/one.py
+++ flask_project/one.py
@@ -1,19 +1,31 @@
 # routing
  
-from flask import Flask
+# from flask import Flask
+
+# app=Flask(__name__)
+# @app.route('/')
+# def hello():
+#    return 'hello world mode'
+
+# @app.route('/products')
+# def product():
+#    return 'this is product page'
+# if __name__=="__main__":
+#     app.run(debug=True )
+    
+#  -------------------------------------------------------------------
+
+from flask import Flask,render_template
 
 app=Flask(__name__)
 @app.route('/')
 def hello():
-   return 'hello world mode'
+   return render_template('index.html')
+   # return 'hello world mode'
 
 @app.route('/products')
 def product():
    return 'this is product page'
 if __name__=="__main__":
     app.run(debug=True )
-    
-#  -------------------------------------------------------------------
-
-   
 
