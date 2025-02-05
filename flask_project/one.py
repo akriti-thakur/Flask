846764fd4a0e1621f03186f6f97c13ab1c9c918a
Akriti Thakur <112692104+akriti-thakur@users.noreply.github.com>
2025-02-05 20:30:43 +0530

    changes

--- flask_project/one.py
+++ flask_project/one.py
@@ -11,7 +11,7 @@
 # def product():
 #    return 'this is product page'
 # if __name__=="__main__":
-#     app.run(debug=True )
+#     app.run(debug=True,port=9000)
     
 #  -------------------------------------------------------------------
 
